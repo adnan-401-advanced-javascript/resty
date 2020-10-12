b576edd280244a7886ffdcb41694254eb30a3736
amuammer <amuammer@students.iugaza.edu.ps>
2020-10-12 21:37:21 +0300

    Updates

---
+++ precache-manifest.fed132845de9f74b45d3e94ce5ae0490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bee85446b09d54c5c9a954848692a50",
+    "url": "/adnan-401-advanced-javascript/resty/index.html"
+  },
+  {
+    "revision": "b4e52ecbc806541496e9",
+    "url": "/adnan-401-advanced-javascript/resty/static/css/main.473ffa78.chunk.css"
+  },
+  {
+    "revision": "f153b639355e899fd6b3",
+    "url": "/adnan-401-advanced-javascript/resty/static/js/2.ca01434d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adnan-401-advanced-javascript/resty/static/js/2.ca01434d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e52ecbc806541496e9",
+    "url": "/adnan-401-advanced-javascript/resty/static/js/main.72645c08.chunk.js"
+  },
+  {
+    "revision": "3a8f3db10cf4ed008c71",
+    "url": "/adnan-401-advanced-javascript/resty/static/js/runtime-main.5c044e66.js"
+  }
+]);
