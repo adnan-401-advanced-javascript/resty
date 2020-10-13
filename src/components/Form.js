d062534a40ba2e0502f0899b21d2cc260cb4c1da
amuammer <amuammer@students.iugaza.edu.ps>
2020-10-13 15:38:24 +0300

    git commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,8 +10,10 @@ export default class Form extends Component {
   this.state = {
     method: '',
     url: '',
+    reqBody: '',
     goMethod: "",
     goUrl: "",
+    goBody: "",
     responseJson: {},
   }
 }
@@ -23,24 +25,30 @@ fetchData = () => {
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify(this.state.reqBody),
   })
    .then(async (res) => {
+     console.log(res);
+     /*
     const obj = {};
     for (let [key, value] of res.headers.entries()) {
       obj[key] = value;
     }
      const resJson = await res.json();
      this.setState({ responseJson: { Headers: obj, Response: resJson }});
+     */
    })
 }
 
 onChangeMethod = e => this.setState({ method: e.target.value })
 onChangeUrl = e => this.setState({ url: e.target.value });
+// onChangeBody = e => this.setState({ reqBody: e.target.value });
 
 printUrlAndMethod = e => {
   e.preventDefault();
   this.setState({ goMethod: this.state.method });
   this.setState({ goUrl: this.state.url });
+  this.setState({ goBody: this.state.reqBody });
   this.fetchData();
 };
 
@@ -51,7 +59,7 @@ render() {
         <br/>
         <br/>
         <br/>
-        <input placeholder="http://" onChange={this.onChangeUrl} />
+        <input name="url" placeholder="http://" onChange={this.onChangeUrl} />
         <br/>
         <input checked type="radio" value="GET" name="method" onChange={this.onChangeMethod} />
         <label htmlFor="GET">GET</label>
@@ -61,6 +69,11 @@ render() {
         <label htmlFor="PUT">PUT</label>
         <input type="radio" value="DELETE" name="method" onChange={this.onChangeMethod} />
         <label htmlFor="DELETE">DELETE</label>
+        <br/>
+        <br/>
+        <textarea name="reqBody" defaultValue={`{ "username": "adnan",
+    "pass": "123"
+ }`}></textarea>
         <br/>
         <button type="submit">Go</button>
       </form>
