2fc53c90e07076cdc003ec34f73a7351c48e7589
amuammer <amuammer@students.iugaza.edu.ps>
2020-10-14 03:10:45 +0300

    finish lab28

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -3,37 +3,31 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from '../App.js';
-// import People from '../people.js';
+import Results from "../components/Results";
+import History from "../components/History";
 
-test('dynamically update my number ', async(done) => {
+test('check render App ', () => {
     render(<App />); // I will have a screen -> get me an element
-    const input = screen.getByTestId('num');
-    const output = screen.getByTestId('output');
-    fireEvent.change(input, {target: {value: 234 }} );
-    expect(output).toHaveTextContent('234');
-    done();
+    const header = screen.getByTestId('header');
+    const body = screen.getByTestId('main');
+    const footer = screen.getByTestId('footer');
+    expect(header).toBeTruthy();
+    expect(body).toBeTruthy();
+    expect(footer).toBeTruthy();
 });
 
-// test('dynamically update my number 2 ',async(done) => {
-//     render(<App />); // I will have a screen -> get me an element
-//     const input = screen.getByTestId('num');
-//     const output = screen.getByTestId('output');
-//     fireEvent.change(input, {target: {value: 234 }} );
-//     expect(output.textContent).toBe('234');
-//     done();
-// });
 
-// test('should render star wars list', async(done)=> {
-//     const people = {
-//         "name1" : "http://swapi.dev/api/people/1",
-//         "name2" : "http://swapi.dev/api/people/2"
-//     }
-//     render(<People people= {people} loading={false}/>);
-//     const items = screen.getAllByRole('listitem');
-//     // console.log(items[0]);
-//     expect(items[0]).toHaveTextContent('name1');
-//     expect(items[0]).toContainHTML('<a href="http://swapi.dev/api/people/1">');
-//     expect(items[1]).toHaveTextContent('name2');
-//     expect(items[1]).toContainHTML('<a href="http://swapi.dev/api/people/2">');
-//     done();
-// })
+test('check render results', () => {
+  const json = { headers: "header", body: "header" }
+  render (<Results json={json} />);
+  const results = screen.getByTestId('results');
+  expect(results).toBeTruthy();
+})
+
+test('check render history', () => {
+  const items = [{ method: "get", url: "https://adnan" }];
+  const selectItem = function (i){};
+  render (<History selectItem={selectItem} items={items} />);
+  const history = screen.getByTestId('history');
+  expect(history).toBeTruthy();
+})
--- src/components/History.js
+++ src/components/History.js
@@ -3,12 +3,10 @@ import React from 'react';
 function History(props) {
   const items = props.items || [];
 
-  console.log("items", items);
-
   return (
     <aside className="App-history">
       <h4>Search History</h4>
-      <ul>
+      <ul data-testid="history" >
         {
           items.map((item, i) =>
             <li key={i}>
